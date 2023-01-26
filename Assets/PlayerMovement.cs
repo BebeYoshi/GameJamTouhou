fd06477947774833424259a26b5080b7296c2765
Cauã dos Santos Rebelo <cauarebelo@gmail.com>
2023-01-26 16:58:10 -0300

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float MovementSpeed = 5;
+
+    public Rigidbody2D rb;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GameJamTouhou
+ GameJamTouhou
