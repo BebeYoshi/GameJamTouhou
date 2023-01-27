21483d91fe7f0085ffe8171693b7b5d6877a1c76
Cauã dos Santos Rebelo <cauarebelo@gmail.com>
2023-01-27 16:31:06 -0300

    Score Snowboard

--- Assets/Script/Jogador/Info_Player.cs
+++ Assets/Script/Jogador/Info_Player.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public static class Info_Player 
 {
     private static int p_coins = 0;
+    private static int p_score_snowboard = 0;
 
     public static int coins{
         get { return p_coins; }
         set { p_coins  = value; }
     }
-    
+
+    public static int score_snowboard
+    {
+        get { return p_score_snowboard; }
+        set { p_score_snowboard = value; }
+    }
+
 }
