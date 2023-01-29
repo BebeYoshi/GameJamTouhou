d909a308e05b17b5f20e3fe3f4036f159046be81
Pedro Augusto <pedro.augusto@illbe.com.br>
2023-01-29 02:03:45 -0300

    hud minigames

--- Assets/Script/Cidade/Interativos/CollisionController_Livro.cs
+++ Assets/Script/Cidade/Interativos/CollisionController_Livro.cs
@@ -7,16 +7,34 @@ using UnityEngine.SceneManagement;
 public class CollisionController_Livro : MonoBehaviour
 {
     public Minigames_UI uiMinigames;
+    public GameObject exclamacaoIMG;
     
     void Start(){
     }
 
-void OnCollisionStay2D(Collision2D col)
+void OnCollisionEnter2D(Collision2D col)
     {
         // Animação de exclamação
+       if(col.gameObject.name == "Player"  && !exclamacaoIMG.activeSelf){
+            exclamacaoIMG.SetActive(true);
+       }
+    }
+
+void OnCollisionStay2D(Collision2D col)
+    {
+
        if(col.gameObject.name == "Player" && Input.GetKey(KeyCode.E) && !uiMinigames.minigamesPanel.activeSelf){
         Debug.Log("abriu");
             uiMinigames.ToggleCanvas();
+       }
+    }
+  
+
+void OnCollisionExit2D(Collision2D col)
+    {
+        // Animação de exclamação
+       if(col.gameObject.name == "Player"  && exclamacaoIMG.activeSelf){
+            exclamacaoIMG.SetActive(false);
        }
     }
   
--- Assets/Script/Cidade/Interativos/CollisionController_NPC.cs
+++ Assets/Script/Cidade/Interativos/CollisionController_NPC.cs
@@ -7,9 +7,19 @@ using UnityEngine.SceneManagement;
 public class CollisionController_NPC : MonoBehaviour
 {
     public Npc_UI uiNpc;
+    public GameObject exclamacaoIMG;
     
     void Start(){
     }
+
+void OnCollisionEnter2D(Collision2D col)
+    {
+        // Animação de exclamação
+       if(col.gameObject.name == "Player"  && !exclamacaoIMG.activeSelf){
+            exclamacaoIMG.SetActive(true);
+       }
+    }
+
 
 void OnCollisionStay2D(Collision2D col)
     {
@@ -17,6 +27,14 @@ void OnCollisionStay2D(Collision2D col)
        if(col.gameObject.name == "Player" && Input.GetKey(KeyCode.E) && !uiNpc.npcPanel.activeSelf){
         Debug.Log("abriu");
             uiNpc.ToggleCanvas();
+       }
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        // Animação de exclamação
+       if(col.gameObject.name == "Player"  && exclamacaoIMG.activeSelf){
+            exclamacaoIMG.SetActive(false);
        }
     }
   
