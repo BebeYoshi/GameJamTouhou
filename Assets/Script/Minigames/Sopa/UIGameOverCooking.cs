a8a2f99b58b2ad45980fb4b2fe03129ea3195ace
Cauã dos Santos Rebelo <cauarebelo@gmail.com>
2023-01-30 10:58:48 -0300

    Update UIGameOverCooking.cs

--- Assets/Script/Minigames/Sopa/UIGameOverCooking.cs
+++ Assets/Script/Minigames/Sopa/UIGameOverCooking.cs
@@ -29,41 +29,41 @@ public class UIGameOverCooking : MonoBehaviour
     {
         if(newHighScore)
         {
-            textoHighScore.SetText("New High Score: " + Info_Player.score_snowballfight.ToString() + "!");
+            textoHighScore.SetText("New High Score: " + Info_Player.score_sopa.ToString() + "!");
         }
         else
         {
-            textoHighScore.SetText("High Score: " + Info_Player.score_snowballfight.ToString() + "!");
+            textoHighScore.SetText("High Score: " + Info_Player.score_sopa.ToString() + "!");
         }
     }
 
     public void DialogueGameOver()
     {
-        if (Info_Player.score_snowballfight <= 2000 && Info_Player.tries_snowballfight <= 5)
+        if (Info_Player.score_sopa <= 2000 && Info_Player.tries_sopa <= 5)
         {
             dialogueGameOver.SetText("Ugh, that little fairy gets on my nerves sometimes...");
         }
-        if (Info_Player.score_snowballfight <= 2000 && Info_Player.tries_snowballfight > 5)
+        if (Info_Player.score_sopa <= 2000 && Info_Player.tries_sopa > 5)
         {
             dialogueGameOver.SetText("STOP DODGING, LET ME SAVE THE SPRING!!");
         }
-        if (Info_Player.score_snowballfight == 0)
+        if (Info_Player.score_sopa == 0)
         {
             dialogueGameOver.SetText("I should try dodging her snowball by pressing DOWN, and shooting her by pressing Mouse1.");
         }
-        if (Info_Player.score_snowballfight >= 2000)
+        if (Info_Player.score_sopa >= 2000)
         {
             dialogueGameOver.SetText("I got a score high enough for the item, also why is she so fast?");
         }
-        if (Info_Player.score_snowballfight >= 3000)
+        if (Info_Player.score_sopa >= 3000)
         {
             dialogueGameOver.SetText("No matter how many times she is defeated, she keeps coming back stronger...");
         }
-        if (Info_Player.score_snowballfight >= 5000)
+        if (Info_Player.score_sopa >= 5000)
         {
             dialogueGameOver.SetText("I have better things to do.");
         }
-        if (Info_Player.score_snowballfight >= 2000 && Info_Player.tries_snowballfight == 1)
+        if (Info_Player.score_sopa >= 2000 && Info_Player.tries_sopa == 1)
         {
             dialogueGameOver.SetText("I got a score high enough for the item on my first try, piece of cake!");
             return;
