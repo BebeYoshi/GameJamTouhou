2c63788035c16c26b4ec1447fcaf3be6790a1123
Cauã dos Santos Rebelo <cauarebelo@gmail.com>
2023-01-30 10:22:05 -0300

    Cidade camera

---
+++ Assets/Script/Minigames/Sopa/ScoreCooking.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreCooking : MonoBehaviour
+{
+
+    public int score;
+    public UIGameOverBall uiGameOver;
+    public GameObject tip;
+    public TMP_Text textoScore;
+
+
+    void Start()
+    {
+        score = 0;
+        textoScore.SetText("Score: " + score.ToString());
+        if(Info_Player.tries_snowballfight == 0 || Info_Player.score_snowballfight < 1000)
+        {
+            StartCoroutine(GiveTip());
+        }
+    }
+
+    public void Score()
+    {
+        score += 200;
+        textoScore.SetText("Score: " + score.ToString());
+    }
+
+    public void FinaldeJogo()
+    {
+        Info_Player.coins += (score / 25);
+        Info_Player.tries_snowballfight++;
+        uiGameOver.ToggleCanvas();
+        if (Info_Player.score_snowballfight < score)
+        {
+            Info_Player.score_snowballfight = score;
+            uiGameOver.HighScore(true);
+        }
+        else
+        {
+            uiGameOver.HighScore(false);
+        }
+    }
+
+    IEnumerator GiveTip()
+    {
+        tip.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        tip.SetActive(false);
+    }
+}
---
+++ Assets/Script/Minigames/Sopa/UIGameOverCooking.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIGameOverCooking : MonoBehaviour
+{
+
+    public TMP_Text textoHighScore;
+
+    public TMP_Text dialogueGameOver;
+
+    public GameObject painel;
+
+    public void ToggleCanvas()
+    {
+        if(!painel.activeSelf)
+        {
+            painel.SetActive(true);
+            DialogueGameOver();
+        }
+        else
+        {
+            painel.SetActive(false);
+        }
+    }
+
+    public void HighScore(bool newHighScore)
+    {
+        if(newHighScore)
+        {
+            textoHighScore.SetText("New High Score: " + Info_Player.score_snowballfight.ToString() + "!");
+        }
+        else
+        {
+            textoHighScore.SetText("High Score: " + Info_Player.score_snowballfight.ToString() + "!");
+        }
+    }
+
+    public void DialogueGameOver()
+    {
+        if (Info_Player.score_snowballfight <= 2000 && Info_Player.tries_snowballfight <= 5)
+        {
+            dialogueGameOver.SetText("Ugh, that little fairy gets on my nerves sometimes...");
+        }
+        if (Info_Player.score_snowballfight <= 2000 && Info_Player.tries_snowballfight > 5)
+        {
+            dialogueGameOver.SetText("STOP DODGING, LET ME SAVE THE SPRING!!");
+        }
+        if (Info_Player.score_snowballfight == 0)
+        {
+            dialogueGameOver.SetText("I should try dodging her snowball by pressing DOWN, and shooting her by pressing Mouse1.");
+        }
+        if (Info_Player.score_snowballfight >= 2000)
+        {
+            dialogueGameOver.SetText("I got a score high enough for the item, also why is she so fast?");
+        }
+        if (Info_Player.score_snowballfight >= 3000)
+        {
+            dialogueGameOver.SetText("No matter how many times she is defeated, she keeps coming back stronger...");
+        }
+        if (Info_Player.score_snowballfight >= 5000)
+        {
+            dialogueGameOver.SetText("I have better things to do.");
+        }
+        if (Info_Player.score_snowballfight >= 2000 && Info_Player.tries_snowballfight == 1)
+        {
+            dialogueGameOver.SetText("I got a score high enough for the item on my first try, piece of cake!");
+            return;
+        }
+    }
+}
