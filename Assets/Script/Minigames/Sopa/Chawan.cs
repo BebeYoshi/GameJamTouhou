02d759c2cc65c85cb554dfa01348300d140995bf
Andre Nakazawa <andrenkzw@gmail.com>
2023-01-29 22:00:26 -0300

    Cursor da Sopa diferente

--- Assets/Script/Minigames/Sopa/Chawan.cs
+++ Assets/Script/Minigames/Sopa/Chawan.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Chawan : MonoBehaviour
 {
+    public Texture2D NormalTexture;
+    public Texture2D GrabTexture;
     public GameObject ingrediente;
     // Start is called before the first frame update
     void Start()
     {
-        
+        NormalTexture = Resources.Load("cursor2", typeof(Texture2D)) as Texture2D;
+        GrabTexture = Resources.Load("cursor1", typeof(Texture2D)) as Texture2D;
+        Cursor.SetCursor(NormalTexture, Vector2.zero, CursorMode.Auto);
     }
 
     // Update is called once per frame
@@ -19,13 +23,15 @@ public class Chawan : MonoBehaviour
 
     private void OnMouseDown()
     {
-       ingrediente.GetComponent<CookingObject>().offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
-       ingrediente.GetComponent<CookingObject>().dragging = true;
-       ingrediente.SetActive(true);
+        Cursor.SetCursor(GrabTexture, Vector2.zero, CursorMode.Auto);
+        ingrediente.GetComponent<CookingObject>().offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        ingrediente.GetComponent<CookingObject>().dragging = true;
+        ingrediente.SetActive(true);
     }
 
     private void OnMouseUp()
     {
+        Cursor.SetCursor(NormalTexture, Vector2.zero, CursorMode.Auto);
         ingrediente.GetComponent<CookingObject>().dragging = false;
         ingrediente.SetActive(false);
     }
